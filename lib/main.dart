fd87caa8055eb002322ed6658826a49eb3029c10
Alyson Takai <shtakai@gmail.com>
2019-09-26 15:10:38 +0900

    Rename file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fuck_provider/widgets/counter_page.dart';
+import 'package:fuck_provider/widgets/counter_widget.dart';
 
 void main() => runApp(MyApp());
 
